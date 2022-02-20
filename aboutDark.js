e11dc9bcd2b6d90ab49a2c5ead9eb0be850c29cc
Diamondw69 <86194579+Diamondw69@users.noreply.github.com>
2022-02-20 23:57:33 +0600

    aaaa

---
+++ aboutDark.js
@@ -0,0 +1,19 @@
+let switchCode= document.getElementById("switchMode");
+switchMode.onclick=function(){
+    let theme= document.getElementById("theme");
+    const navbar=document.querySelector('#navbar');
+
+
+    if(theme.getAttribute("href")=="css/about.css"){
+        theme.href="darkAbout.css";
+        navbar.classList.remove('navbar-light','bg-light');
+        navbar.classList.add('navbar-dark','bg-dark');
+        document.querySelector('.themetoggle span').textContent = 'dark_mode';
+
+    }else{
+        theme.href="css/about.css"
+        navbar.classList.remove('navbar-dark','bg-dark');
+        navbar.classList.add('navbar-light','bg-light');
+        document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+    }
+}
